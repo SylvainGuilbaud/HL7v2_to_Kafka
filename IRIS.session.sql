5c8121f8c24e4641c65ab106909a8eca9b9193e1
SylvainGuilbaud <sylvain.guilbaud@intersystems.com>
2024-04-29 18:25:07 +0200

    init

--- IRIS.session.sql
+++ IRIS.session.sql
@@ -1,3 +1,11 @@
+-- data sent to Kafka
 SELECT 
 ID, key, topic, value
 FROM HL7v2toKafka_msg.kafka
+order by id desc;
+
+-- data from Kafka back into IRIS
+SELECT 
+ID, key, topic, value, ts
+FROM data.kafka
+order by id desc;
